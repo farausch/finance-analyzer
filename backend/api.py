76fbdb5f9482a6cf81e22ae227548a97b0954cd8
Fabian Rausch <fabian.rausch@pwc.com>
2024-11-14 18:52:52 +0100

    Restructure to backend and add label service

--- backend/api.py
+++ backend/api.py
@@ -4,10 +4,12 @@ from config.db_config import engine, SessionLocal
 from services.import_service import ImportService
 from config.import_config import Provider
 from model import Finance_Transaction, Base
+from services.label_service import LabelService
 
 Base.metadata.create_all(bind=engine)
 
 import_service = ImportService()
+label_service = LabelService()
 
 app = FastAPI()
 
@@ -18,11 +20,19 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/finance_transactions/")
+@app.get("/transactions/")
 def read_finance_transactions(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     transactions = db.query(Finance_Transaction).offset(skip).limit(limit).all()
     return transactions
 
-@app.post("/import_csv/")
+@app.post("/transactions/import-csv/")
 async def import_csv(provider: Provider, file: UploadFile = File(...), db: Session = Depends(get_db)):
     return await import_service.import_data(provider, file.filename, file, db)
+
+@app.post("/transactions/add-label")
+def add_label_to_transaction(transaction_id: int, label_id: int, db: Session = Depends(get_db)):
+    return label_service.add_label_to_transaction(db, transaction_id, label_id)
+
+@app.post("/labels/create/")
+def create_label(name: str, display_name: str, db: Session = Depends(get_db)):
+    return label_service.create_label(db, name, display_name)
---
+++ backend/services/label_service.py
@@ -0,0 +1,16 @@
+from model import Finance_Label, Finance_Transaction_Label
+
+class LabelService:
+
+    def __init__(self):
+        pass
+
+    def create_label(self, db, name, display_name):
+        db.add(Finance_Label(name=name, display_name=display_name))
+        db.commit()
+        return {"message": f"Successfully created label {name}."}
+
+    def add_label_to_transaction(self, db, transaction_id, label_id):
+        db.add(Finance_Transaction_Label(transaction_id=transaction_id, label_id=label_id))
+        db.commit()
+        return {"message": f"Successfully added label {label_id} to transaction {transaction_id}."}
